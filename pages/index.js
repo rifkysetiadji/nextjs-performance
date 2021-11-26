efa788c9f5026af7c0df84a76d8b59b4f9afbb9d
rifkysetiadji <rifkywikrama.s@gmail.com>
2021-11-26 16:42:53 +0700

    add carousel

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,32 @@ export default function Home() {
     dots: true,
     infinite: true,
     speed: 500,
-    centerPadding:"370px",
+    centerPadding:"350px",
     slidesToShow: 1,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive:[
+      {
+          breakpoint: 600,
+          settings: {
+              centerPadding: "20px",
+              arrows:false,
+              // nextArrow: null,
+              // prevArrow: null,
+              dots:true,
+              // dotsClass: `slick-dots ${style.dots_mobile}`,
+          }
+        },
+  ]
   };
   return (
-    <div style={{width:'90%'}}>
+    <div style={{width:'100%'}}>
        <Slider {...settings}>
-       <Image width={958} height={392} src="/wts1.jpg" alt="img" layout="fixed"  />
+       <Image width={958} height={392} src="/wts1.jpg" alt="img" layout="responsive"  />
+       <Image width={958} height={392} src="/wts2.jpg" alt="img" layout="responsive"  />
+       <Image width={958} height={392} src="/wts3.jpg" alt="img" layout="responsive"  />
+       {/* <Image width={958} height={392} src="/wts1.jpg" alt="img" layout="fixed"  />
        <Image width={958} height={392} src="/wts2.jpg" alt="img" layout="fixed"  />
-       <Image width={958} height={392} src="/wts3.jpg" alt="img" layout="fixed"  />
+       <Image width={958} height={392} src="/wts3.jpg" alt="img" layout="fixed"  /> */}
        </Slider>
        
        {/* <div style={{width:640,height:292,position:'relative'}}> */}
