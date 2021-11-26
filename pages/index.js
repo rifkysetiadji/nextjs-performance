6da32153de39b4e4ba4ad12ed8aaf2a006a9ab41
rifkysetiadji <rifkywikrama.s@gmail.com>
2021-11-26 16:20:02 +0700

    add carousel

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,21 @@ import Slider from "react-slick";
 import Image from 'next/image'
 export default function Home() {
   const settings = {
+    className: "banner-carousel",
+        centerMode: true,
     dots: true,
     infinite: true,
     speed: 500,
+    centerPadding:"370px",
     slidesToShow: 1,
     slidesToScroll: 1
   };
   return (
-    <div style={{width:'50%'}}>
+    <div style={{width:'90%'}}>
        <Slider {...settings}>
        <Image width={958} height={392} src="/wts1.jpg" alt="img" layout="fixed"  />
        <Image width={958} height={392} src="/wts2.jpg" alt="img" layout="fixed"  />
+       <Image width={958} height={392} src="/wts3.jpg" alt="img" layout="fixed"  />
        </Slider>
        
        {/* <div style={{width:640,height:292,position:'relative'}}> */}
