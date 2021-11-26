8e1b60f0bf72336bc23e329ff02bef23c8aa332b
rifkysetiadji <rifkywikrama.s@gmail.com>
2021-11-26 15:45:51 +0700

    add carousel

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,10 @@ export default function Home() {
     slidesToScroll: 1
   };
   return (
-    <div style={{width:'50%',height:292}}>
+    <div style={{width:'50%'}}>
        <Slider {...settings}>
-       <Image width={640} height={292} src="/wts1.jpg" alt="img" layout="fixed"  />
-       <Image width={640} height={292} src="/wts2.jpg" alt="img" layout="fixed"  />
+       <Image width={958} height={392} src="/wts1.jpg" alt="img" layout="fixed"  />
+       <Image width={958} height={392} src="/wts2.jpg" alt="img" layout="fixed"  />
        </Slider>
        
        {/* <div style={{width:640,height:292,position:'relative'}}> */}
