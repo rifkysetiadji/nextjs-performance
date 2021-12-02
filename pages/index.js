3e75b771f53dcd4f23090e2fd0c0dcd50cc441f0
rifkysetiadji <rifkywikrama.s@gmail.com>
2021-12-02 14:01:23 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   const settings = {
     className: "banner-carousel",
-        centerMode: true,
+        // centerMode: true,
     dots: true,
     infinite: true,
     speed: 500,
